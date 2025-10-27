1182a94c05badd540f40b8c43fe2b98e2e26f589
java4life <java4life@gmail.com>
2025-10-27 19:05:58 +0100

    Add GitHub Actions workflow for Eleventy build and deploy process

--- eleventy.config.js
+++ eleventy.config.js
@@ -19,6 +19,7 @@ module.exports = function(eleventyConfig) {
       output: "_site",
       includes: "_includes",
       data: "_data"
-    }
+    },
+    pathPrefix: process.env.ELEVENTY_PATH_PREFIX || "/"
   };
 };
